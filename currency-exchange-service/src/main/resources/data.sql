1fc080472493fd8cea038d1aed949589a324e70f
Ramanjit Kaur <ramanjit.jawandha@gmail.com>
2019-08-18 18:55:36 -0400

    4th currency-exchange-commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value (id,currency_from ,currency_to, conversion_multiple,port)
+values(1001, 'USD','IN',65,0);
+
+insert into exchange_value (id,currency_from ,currency_to, conversion_multiple,port)
+values(1002, 'USD','IN',45,0);
+
